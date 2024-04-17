420d844fa9cef70103aa3eccfdcfbf58001f11be
Kunle Li <1041593558@qq.com>
2024-04-17 10:19:19 -0400

    feat: volume button; music cover

--- melodyhub/musicplay/static/musicplay/js/audio_play.js
+++ melodyhub/musicplay/static/musicplay/js/audio_play.js
@@ -4,8 +4,10 @@ document.querySelectorAll('.play-btn').forEach(btn => {
         var currentSong = document.getElementById('currentSong');
         var playStatusBar = document.getElementById('play-status-bar');
         var playPauseButton = document.querySelector('.play');
+        var musicCover = document.getElementById('music-cover');
         audioPlayer.src = this.dataset.src;
         currentSong.textContent = this.parentElement.querySelector('.music-title').textContent + ' - ' + this.parentElement.querySelector('.music-artist').textContent;
+        musicCover.src = this.dataset.cover;
         audioPlayer.play();
         playPauseButton.classList.remove('play');
         playPauseButton.classList.add('pause');
