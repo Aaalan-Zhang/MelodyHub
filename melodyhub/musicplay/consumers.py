52b20c2676f308e23408bfe625f2008088614969
Ubuntu <ubuntu@ip-172-31-82-131.ec2.internal>
2024-04-17 15:50:15 +0000

    cloud deployment change

--- melodyhub/musicplay/consumers.py
+++ melodyhub/musicplay/consumers.py
@@ -1,7 +1,7 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 class SyncConsumer(AsyncWebsocketConsumer):
-    # active_connections = 0
+    active_users = {}
 
     async def connect(self):
         # self.active_connections += 1
@@ -11,13 +11,33 @@ class SyncConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_add(
             self.group_name, self.channel_name
         )
-        await self.broadcast_active_connections()
+
         await self.accept()
+        session_key = self.scope['session'].session_key
+
+        if self.group_name not in self.active_users:
+            self.active_users[self.group_name] = [session_key]
+        elif session_key not in self.active_users[self.group_name]:
+            self.active_users[self.group_name].append(session_key)
+        # print(self.active_users)
+
+        await self.broadcast_active_connections()
+
+
+
+
 
 
 
 
     async def disconnect(self, close_code):
+        session_key = self.scope['session'].session_key
+        if self.group_name in self.active_users and \
+           session_key in self.active_users[self.group_name]:
+            # Remove my session key from the list
+            self.active_users[self.group_name] = list(filter(
+                lambda x: x != session_key,
+                self.active_users[self.group_name]))
         # self.active_connections -= 1
         await self.broadcast_active_connections()
         await self.channel_layer.group_discard(
@@ -53,7 +73,7 @@ class SyncConsumer(AsyncWebsocketConsumer):
 
 
     async def sync_message(self, event):
-        print(event['msg_content'])
+        # print(event['msg_content'])
         await self.send(text_data=json.dumps({
             'msg_type': event['msg_type'],
             'msg_content': event['msg_content']
@@ -70,7 +90,9 @@ class SyncConsumer(AsyncWebsocketConsumer):
                 self.group_name,
             {
                 'type': 'active_connections_message',
-                'message': len(self.channel_layer.groups.get(self.group_name, {}))
+                # 'message': len(self.channel_layer.groups.get(self.group_name, {}))
+                'message': len(self.active_users[self.group_name])
+
             }
         )
 
